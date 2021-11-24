36506f6cc4e64854e1564558ed49acfe6aa962a9
sotharoth <sotharoth@hotmail.com>
2021-11-24 21:27:41 +0700

    Update Base.tsx

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,7 +1,7 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
-import { Footer } from './Footer';
+
+
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 import {
