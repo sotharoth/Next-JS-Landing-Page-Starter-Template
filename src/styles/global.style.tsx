c6c137afc56de4cf4c72e409ffcbe98f3f72a036
sotharoth <sotharoth@hotmail.com>
2021-11-24 21:24:20 +0700

    Update global.style.tsx

--- src/styles/global.style.tsx
+++ src/styles/global.style.tsx
@@ -260,47 +260,6 @@ export const GlobalStyle = createGlobalStyle(({ theme }) =>
         height: '350px !important',
       },
     },
-    '.rc-table-fixed-header .rc-table-scroll .rc-table-header': {
-      marginBottom: '0 !important',
-      paddingBottom: '0 !important',
-
-      th: {
-        padding: '8px 20px',
-      },
-    },
-
-    '.drawer-content-wrapper': {
-      '*:focus': {
-        outline: 'none',
-      },
-    },
-
-    '.rc-table-content': {
-      border: 0,
-    },
-
-    '#modal-root': {
-      zIndex: 999999,
-      position: 'relative',
-    },
-
-    '.drawer': {
-      outline: 0,
-      boxShadow: 'none',
-    },
-    '.srOnly': {
-      border: '0 !important',
-      clip: 'rect(1px, 1px, 1px, 1px) !important',
-      clipPath: 'inset(50%) !important',
-      height: '1px !important',
-      margin: '-1px !important',
-      overflow: 'hidden !important',
-      padding: '0 !important',
-      position: 'absolute !important',
-      width: '1px !important',
-      whiteSpace: 'nowrap !important',
-    },
-    // @ts-ignore
-    //  ...theme.globals,
+  
   })
 );
